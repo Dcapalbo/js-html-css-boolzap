44d51b8aaaf5ba5f3989e3da9a5db7154b44aed9
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-09 16:57:16 +0200

    append-problem
    js-html-css-boolzap

--- js/main.js
+++ js/main.js
@@ -2,19 +2,18 @@ $(document).ready(function() {
   // make a function to insert a text from the input inside of the message-area
   function sendSms() {
     var sms = $(".footer-message-area input").val();
-
     if (sms != "") {
       $(".footer-message-area input").val("");
       // make a copy of the input
-      var newRow = $(".message_row").clone();
-      // inserisci il testo text nel cloudText
-      newRow.find("p").text(sms);
-      // inserisci la classe sent
-      newRow.addClass(".green-box");
+      var template = $(".template .message-row").clone();
+      // insert the text inside of the p
+      template.find("p").text(sms);
+      // insert the class
+      template.addClass(".green-box");
       // append the object inside of the main message area ul
-      $("ul .main-message-screen").append(newRow);
+      $("ul .main-message-screen").append(template);
     }
-  };
+  }
   // make a function to send the text with a press of a button on the keyboard
   $(".footer-message-area input").keydown(function(event) {
     if (event.which == 13) {
