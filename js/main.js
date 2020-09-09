522bc80bdaf5545ffeb0dc953964cb431a8ea686
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-09 19:22:00 +0200

    asincrono-function-done
    js-html-css-boolzap

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
   // make a function to insert a text from the input inside of the message-area
+
   function sendSms() {
+    // make some variables for the date and the hours
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    if (minutes < 10) {
+      var time = hours + " : " + " 0" + minutes;
+    } else {
+      var time = hours + " : " + minutes;
+    }
+    // make a variable for the input value
     var sms = $(".footer-message-area input").val();
     if (sms != "") {
       $(".footer-message-area input").val("");
@@ -8,16 +19,32 @@ $(document).ready(function() {
       var template = $(".template .message-row").clone();
       // insert the text inside of the p
       template.find("p").text(sms);
+      template.find("span").text(time);
       // insert the class
-      template.addClass("green-box");
-      // append the object inside of the main message area ul
+      template.addClass("send");
+      // append the object inside of the main message screen
       $(".main-message-screen").append(template);
     }
   }
+
+  function getReply(text) {
+    // make a variable to clone the template
+    var template = $(".template .message-row").clone();
+    // chose the tagname to insert the text
+    template.find("p").text(text);
+    // attach the class tot he template
+    template.addClass("automatic");
+    // append the text inside the new template
+    $(".main-message-screen").append(template);
+  }
+  // make an asincron function to get an automatic reply
   // make a function to send the text with a press of a button on the keyboard
   $(".footer-message-area input").keydown(function(event) {
     if (event.which == 13) {
       sendSms();
+      setTimeout(function(){
+        getReply("ok!")
+      }, 1000);
     }
   });
 });
