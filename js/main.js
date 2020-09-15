ad845939e4dd9070f224af13129a7c4d3e80092f
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-16 00:28:40 +0200

    dropdown-list-changes-random-reply-done
    js-html-css-boolzap

--- js/main.js
+++ js/main.js
@@ -1,64 +1,42 @@
 $(document).ready(function() {
-  //Invoke getTime function and press it inside of the differents span tagName inside the DOM
-  document.getElementsByTagName("span")[12].innerHTML = getTime();
-  document.getElementsByTagName("span")[13].innerHTML = getTime();
-  document.getElementsByTagName("span")[14].innerHTML = getTime();
-  document.getElementsByTagName("span")[15].innerHTML = getTime();
-  document.getElementsByTagName("span")[16].innerHTML = getTime();
-  document.getElementsByTagName("span")[17].innerHTML = getTime();
-  document.getElementsByTagName("span")[18].innerHTML = getTime();
-  document.getElementsByTagName("span")[19].innerHTML = getTime();
-  document.getElementsByTagName("span")[20].innerHTML = getTime();
-  document.getElementsByTagName("span")[21].innerHTML = getTime();
-  document.getElementsByTagName("span")[22].innerHTML = getTime();
-  document.getElementsByTagName("span")[23].innerHTML = getTime();
-  document.getElementsByTagName("span")[24].innerHTML = getTime();
-  document.getElementsByTagName("span")[25].innerHTML = getTime();
-  document.getElementsByTagName("span")[26].innerHTML = getTime();
-  document.getElementsByTagName("span")[27].innerHTML = getTime();
-  document.getElementsByTagName("span")[28].innerHTML = getTime();
-  document.getElementsByTagName("span")[29].innerHTML = getTime();
-  document.getElementsByTagName("span")[30].innerHTML = getTime();
-  document.getElementsByTagName("span")[31].innerHTML = getTime();
-  document.getElementsByTagName("span")[32].innerHTML = getTime();
-  document.getElementsByTagName("span")[33].innerHTML = getTime();
-  //make a variable of the hours usefull for the contact list
-  // var hours = [10:32, 23:54, 04:44, 02:34, 08:32, 11:34, 18:45, 19:43, 22:25, 17:34, 21:25];
-  // // console.log(hours);
-  // var randomHours = Math.floor(Math.random() * hours.length);
-  // console.log(randomHours);
+  //Invoke getTime function and press it inside of the differents span and p tagName inside the DOM with a class
   // myFunctions
   $(".footer-message-area input").mouseenter(
   function() {
     // make some variables for an elastic coding
-    // when the mouse enter inside the input box, show the plane and hide the microphone
     var plane = $(".fa-paper-plane")
     var microphone = $(".fa-microphone")
+    // when the mouse enter inside the input box, show the plane and hide the microphone
       plane.removeClass("d-none");
       microphone.addClass("d-none");
       sendSms();
     });
+
     $(".footer-message-area input").mouseleave(
     function() {
       // make some variables for an elastic coding
-      // when the mouse enter inside the input box, show the plane and hide the microphone
       var plane = $(".fa-paper-plane")
       var microphone = $(".fa-microphone")
+      // when the mouse enter inside the input box, show the plane and hide the microphone
         plane.addClass("d-none");
         microphone.removeClass("d-none");
       });
+
     //make a function which, at the click will show the dropdown-list
     $(document).on( "click", ".angle-down", function() {
        $(this).next(".dropdown-list").removeClass("d-none");
      });
+
     //make a function which, at the click will remove the dropdown-list
     $(document).on( "click", ".dropdown-list .cm", function() {
        $(this).parents(".message-row").remove();
      });
+
     //make a function which, at the click will add the class, display none
     $(document).on( "mouseleave", ".message-text", function() {
        $(this).find(".dropdown-list").addClass("d-none");
      });
+
   //make a function to get the actual time from the client computer
   function getTime() {
     var date = new Date();
@@ -70,7 +48,8 @@ $(document).ready(function() {
     } else {
       var time = hours + " : " + minutes;
     } return time;
-  };
+  }; $("span").append(getRandomHours());
+
   // make a function to insert a text from the input inside of the message-area
   function sendSms(attribute) {
     // make a variable for the value of the input
@@ -87,6 +66,7 @@ $(document).ready(function() {
       template.addClass("send");
       // append the object inside of the main message screen
       $(".main-message-area[data-sms="+attribute+"]").append(template);
+      // resetting the value inside the input
       $(".footer-message-area input").val("");
     }
   };
@@ -104,6 +84,7 @@ $(document).ready(function() {
     $(".main-message-area[data-sms="+attribute+"]").append(template);
     $(".header-message-area .info-contact p").text("ultimo accesso alle ore: " + getTime());
   };
+
   // make a function to send the text with a press of a button on the keyboard
   $(".footer-message-area input").keydown(function(event) {
     if (event.which == 13) {
@@ -111,12 +92,11 @@ $(document).ready(function() {
       sendSms(attrContact);
       // make an asincron function and invoke it to get an automatic reply
       setTimeout(function(){
-        var answers = ["Ok","Ci vediamo presto!","Bene","Ciao!","Buona giornata!","Benissimo","Sì", "tranquillo","Perfetto!","Sicuramente","Sono d'accordo","Sul serio?","Ci sentiamo più tardi", "ok?"];
-        var randomAnswers = Math.floor(Math.random() * answers.length);
-        getReply(randomAnswers, attrContact);
+        getReply(getRandomReply(),attrContact);
       }, 2000);
     }
   });
+
   $(".search-bar input").keyup(function(){
       // make a variable to save the input
       var words = $(".search-bar input").val();
@@ -132,6 +112,7 @@ $(document).ready(function() {
         }
       });
     });
+
     $(".contact-template").click(function(){
        // removing the active class from every contact-template and message-contact-    area
        $(".contact-template").removeClass("active");
@@ -141,14 +122,46 @@ $(document).ready(function() {
        // make a variable for data-contact and data-sms
        var attrContact = $(this).attr("data-contact");
        // apply the active class at the chat with the same value of contacts
-       $(".main-message-area[data-sms="+attrContact+"]").addClass("active");
+       $(".main-message-area[data-sms="+ attrContact +"]").addClass("active");
        // make a copy of the img
        var contactImg = $(this).find(".img-wrapper img").clone();
-       // make a copy of the name
+       // make a lecture of the name
        var contactName = $(this).find(".info-contact-name").text();
        // insert the value inside of the header message area
        $(".header-message-area").find(".img-wrapper").html(contactImg);
        $("#changeh4").text(contactName);
      });
-
- });
+     // make a function to randomize some customize answers inside of a for cicle
+     function getRandomReply() {
+      var answers = [
+       "Ok!",
+       "Ci vediamo presto!",
+       "Bene",
+       "Ciao!",
+       "Buona giornata!",
+       "Benissimo",
+       "Sì", "tranquillo",
+       "Perfetto!",
+       "Sicuramente!",
+       "Sono d'accordo",
+       "Sul serio?",
+       "Ci sentiamo più tardi",
+       "ok?"];
+       for (var i = 0; i < answers.length; i++) {
+         var randomAnswers = Math.floor(Math.random() *     answers.length);
+       }
+       return answers[randomAnswers];
+     }
+      // make a function to randomize some customize hours inside of a for cicle
+     function getRandomHours() {
+      var hours = [
+        "10:32", "23:54", "04:44",
+        "02:34", "08:32", "08:33",
+        "11:34", "18:45", "19:43",
+        "22:25", "17:34", "21:25"];
+       for (var i = 0; i < hours.length; i++) {
+         var randomHours = Math.floor(Math.random() *     hours.length);
+       }
+       return hours[randomHours];
+     }
+});
